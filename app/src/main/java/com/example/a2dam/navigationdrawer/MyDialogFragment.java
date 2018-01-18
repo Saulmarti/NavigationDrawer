521ffcbd0fd5390cb4488a77f4d9a38c5ea0f441
Saulmarti <saulmartivila@gmail.com>
2018-01-18 19:22:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2dam/navigationdrawer/MyDialogFragment.java
@@ -0,0 +1,47 @@
+package com.example.a2dam.navigationdrawer;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by 2dam on 18/01/2018.
+ */
+
+public class MyDialogFragment extends DialogFragment {
+
+    public MyDialogFragment(){
+    }
+
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return createSimpleDialog();
+    }
+
+    public AlertDialog createSimpleDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Acerca de")
+                .setView(R.layout.dialog_layout)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Acciones
+                            }
+                        });
+
+        return builder.create();
+    }
+
+
+}
